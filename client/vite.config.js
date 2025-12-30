f25c1e514d6103f389e96e88b2c96313d633035e
Duc Tri Dinh <dinhductri999@gmail.com>
2025-12-30 22:21:26 +0200

    Enable restart unless-stopped for dev

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true, 
+    strictPort: true,
+    port: 5173, 
+  }
 })
